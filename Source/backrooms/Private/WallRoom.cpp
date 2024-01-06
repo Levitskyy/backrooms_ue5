4a76f7a8bc69584962ddcee25d664eedd46b56ad
DevilishSasuke <petrovich_uasya@mail.ru>
2024-01-07 01:25:12 +0300

    fixed corner column spawn

--- Source/backrooms/Private/WallRoom.cpp
+++ Source/backrooms/Private/WallRoom.cpp
@@ -13,6 +13,8 @@ AWallRoom::AWallRoom()
 {
     WallStaticMesh = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Walls"));
     ColumnStaticMesh = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Columns"));
+    RootComponent = WallStaticMesh;
+    ColumnStaticMesh->SetupAttachment(RootComponent);
 }
 
 void AWallRoom::InstantiateWallsByGraph()
@@ -28,28 +30,42 @@ void AWallRoom::InstantiateWallsByGraph()
     int secondIdxs[2];
     FRandomStream randStream(Seed);
 
+
+    FVector wallOffset(-GridSize, -3 * GridSize, 0);
+    float WallSize = Size / GridSize;
     for (int k = 0; k < GridSize * GridSize * GridSize * GridSize; ++k) {
         int i = k / (GridSize * GridSize);
         int j = k % (GridSize * GridSize);
-
-        if (j >= i || !wallGraph[k]) continue;
-        if (randStream.GetFraction() < SkipWallSpawnChance) continue;
 
         firstIdxs[0] = i / GridSize;
         firstIdxs[1] = i % GridSize;
         secondIdxs[0] = j / GridSize;
         secondIdxs[1] = j % GridSize;
 
-        bool Rotate = abs(i - j) != 1;
-        Rotate = WallRotationAdjustment ? !Rotate : Rotate;
-        float zRotation = Rotate ? 90 : 0;
-
-        float WallSize = Size / GridSize;
         FVector firstRoomCenter(
             firstIdxs[0] * WallSize + 0.5 * WallSize,
             firstIdxs[1] * WallSize + 0.5 * WallSize,
             WallHeightAdjustment / 2
         );
+
+        if ((i >= GridSize) && (j == 0)) {
+            if (((i + 1) % GridSize) > 0) {
+                if (IsHereCorner(wallGraph, i)) {
+                    FRotator curRotation(0, 0, 0);
+                    FVector curTranslateVector(firstRoomCenter.X - 2.5 * WallSize, firstRoomCenter.Y - WallSize * 1.5, firstRoomCenter.Z);
+                    FVector curScale(1, 1, 1);
+                    FTransform curTransform(curRotation, curTranslateVector, curScale);
+                    ColumnTransforms.Emplace(curTransform);
+                }
+            }
+        }
+
+        if (j >= i || !wallGraph[k]) continue;
+        if (randStream.GetFraction() < SkipWallSpawnChance) continue;
+
+        bool Rotate = abs(i - j) != 1;
+        Rotate = WallRotationAdjustment ? !Rotate : Rotate;
+        float zRotation = Rotate ? 90 : 0;
 
         FVector secondRoomCenter(
             secondIdxs[0] * WallSize + 0.5 * WallSize,
@@ -66,18 +82,6 @@ void AWallRoom::InstantiateWallsByGraph()
         FVector scale(1, 1, 1);
         FTransform transform(rotation, translateVector, scale);
         WallTransforms.Emplace(transform);
-
-        if ((firstIdxs[0] * GridSize >= 1) && (firstIdxs[1] == 0)) {
-            int32 globalIndex = firstIdxs[0] * GridSize + firstIdxs[1];
-            if ((globalIndex + 1) % GridSize != 0) {
-                if (IsHereCorner(wallGraph, globalIndex)) {
-                    FRotator curRotation(0, 0, 0);
-                    FVector curTranslateVector(firstRoomCenter.X + WallSize * 0.5, firstRoomCenter.Y - WallSize * 0.5, firstRoomCenter.Z);
-                    FTransform curTransform(curRotation, curTranslateVector, scale);
-                    ColumnTransforms.Emplace(curTransform);
-                }
-            }
-        }
     }
 
 
@@ -96,17 +100,26 @@ bool AWallRoom::IsHereCorner(TArray<bool>& graph, int32 thirdRoomId) {
     int32 wall12 = secondRoomId * sqrGridSize + firstRoomId;
     int32 wall24 = secondRoomId * sqrGridSize + fourthRoomId;
 
-    if ((graph[wall13] && graph[wall34]) ||
-        (graph[wall12] && graph[wall24]))
-        return false;
-    
-    if ((graph[wall13] && graph[wall12]) ||
-        (graph[wall13] && graph[wall34]) ||
-        (graph[wall24] && graph[wall12]) ||
-        (graph[wall24] && graph[wall34]))
-        return true;
+    UE_LOG(LogTemp, Log, TEXT("Rooms: %d %d %d %d"), firstRoomId, secondRoomId, thirdRoomId, fourthRoomId)
+    UE_LOG(LogTemp, Log, TEXT("Indexes: %d %d %d %d"), wall13, wall34, wall12, wall24)
+    UE_LOG(LogTemp, Warning, TEXT("wall13, wall34, wall12, wall24: %s %s %s %s\n\n\n"), 
+        (graph[wall13] ? TEXT("true") : TEXT("false")),
+        (graph[wall34] ? TEXT("true") : TEXT("false")),
+        (graph[wall12] ? TEXT("true") : TEXT("false")),
+        (graph[wall24] ? TEXT("true") : TEXT("false")));
 
-    return false;
+
+    if ((graph[wall13] && graph[wall24]) ||
+        (graph[wall12] && graph[wall34]))
+        return false;
+
+    int wallCounter = 0;
+    if (graph[wall13]) wallCounter++;
+    if (graph[wall34]) wallCounter++;
+    if (graph[wall12]) wallCounter++;
+    if (graph[wall24]) wallCounter++;
+
+    return wallCounter == 2;
 }
 
 void AWallRoom::OnRep_Seed()
